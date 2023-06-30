8384c633a8c1ccd7f62f7418bad3b56557ccdbb4
Tamagoyaki0502 <130971075+Tamagoyaki0502@users.noreply.github.com>
2023-06-30 16:50:31 +0900

    Create SearchData.tsx
    絞り込み（タイトル等）

---
+++ Jigen/Myfunction/SearchData.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import db from '../firebase'
+
+import { doc, getDocs ,collection} from "firebase/firestore";
+import { query,where} from "firebase/firestore";
+
+
+
+var collectionId = "users";
+var documentId = "123";
+
+export type MyType = [{
+    aaa: string;
+    bbb: string;
+    ccc: string;
+
+}];
+export async function SearchGetDatas(collectionId: string ,field:any,value:any): Promise<any[]> {
+
+
+    const q = query(collection(db, collectionId), where(field, "==", value)/**/ );
+    const querySnapshot = await getDocs(q);
+
+    const posts = querySnapshot.docs.map((doc) => doc.data());
+    return posts;
+}
