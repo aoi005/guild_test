8f698862c316ee6e0c134c69d8454d3e20a2a2f8
aoi005 <130361819+aoi005@users.noreply.github.com>
2023-09-28 11:48:35 +0900

    afiriait script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-         <script src="https://adm.shinobi.jp/s/b1b001aede425ac55f0ae62cf18e18f6"></script>
+         <Script src="https://adm.shinobi.jp/s/b1b001aede425ac55f0ae62cf18e18f6"></Script>
         <Main />
         <NextScript />
       </body>
