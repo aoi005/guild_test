adcefa691668e174807bd1494645d93cb50e787f
aoi005 <130361819+aoi005@users.noreply.github.com>
2023-11-24 11:22:50 +0900

    cs test3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,7 @@ export default function Home() {
   return (
     <MainLayout>
     <head>  
+    <meta name="google-adsense-account" content="ca-pub-2205391985510668"/>
       <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2205391985510668"
