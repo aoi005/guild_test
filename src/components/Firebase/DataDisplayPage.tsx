05e7bcb327269b61c8cb73de668f5f22ae52de07
aoi005 <130361819+aoi005@users.noreply.github.com>
2023-12-06 10:00:01 +0900

    name

--- src/components/Firebase/DataDisplayPage.tsx
+++ src/components/Firebase/DataDisplayPage.tsx
@@ -239,7 +239,7 @@ export default function DataDisplayPage() {
               </h3>
             </div>
             <div>
-              <h5>name: {item.name}</h5>
+              <h5>Name: {item.name}</h5>
               <br></br>
                <p key={item.id} className={styles.text}>{item.detail}</p>
               <br></br>
